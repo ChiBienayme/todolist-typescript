d345267fb9e4480efd7a3902986a3529bd5da1ee
Ha Chi HOANG <chibienayme@gmail.com>
2023-09-29 16:33:25 +0200

    reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import styles from './app.module.scss'
 
 function App() {
     return (
         <>
-            <div className={styles.app}>
-                <h1>app</h1>
+            <div>
             </div>
         </>
     )
