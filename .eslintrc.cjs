570e7d6cef4420c6fbd666dcb8c281191a6f9f23
Ha Chi HOANG <chibienayme@gmail.com>
2023-09-29 16:18:03 +0200

    creating

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+    parser: '@babel/eslint-parser', // Use Babel parser for parsing JavaScript/React code
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+    },
+}
+
